20ef6fc89ce023d768ce48f8adaad3301fe4a1e2
pulsarchief <145131830+pulsarchief@users.noreply.github.com>
2024-07-24 00:58:50 +0530

    project completed , final commit
    logic is completed , tested everything works well
    any discovered issue will not be fixed

--- script.js
+++ script.js
@@ -1,25 +1,29 @@
-const reset_btn = document.querySelector("#reset");
-const analyze_btn = document.querySelector("#analyze");
-const count_display = document.querySelector("#number");
-const body = document.querySelector("body");
+/**
+ *
+ * @type Array
+ */
+let wordList = [];
+/**
+ *
+ * @type Array
+ */
+let occurrenceList = [];
 /**
  * @type HTMLInputElement
  */
 const text_area = document.querySelector("#text_input");
-/**
- * @type HTMLInputElement
- */
+const reset_btn = document.querySelector("#reset");
+const analyze_btn = document.querySelector("#analyze");
+const count_display = document.querySelector("#number");
+const body = document.querySelector("body");
+let tbody = document.createElement("tbody");
 const remark = document.createElement("p");
 const data = document.querySelector("#data");
 const footer = document.querySelector("footer");
 const buttons = document.querySelector("#buttons");
-let list = document.createElement("ol");
+const table = document.querySelector("#main_table");
 
-list.style.marginLeft = "90px";
-list.style.marginRight = "90px";
-list.style.textAlign = "center";
-list.style.boxSizing = "border-box";
-
+text_area.focus();
 text_area.value = "";
 
 remark.textContent = "2048 Characters Has  Been Entered";
@@ -46,21 +50,33 @@ text_area.addEventListener("input", () => {
 
 analyze_btn.addEventListener("click", () => {
     analyze();
+    table.style.display = "table";
 });
 
 function analyze() {
     let text = text_area.value.toLocaleLowerCase();
     reset();
     let arr = text.split(/[ .!?,_/:;\r\n]+/).filter(Boolean);
-    let set = new Set(arr.sort());
-    set.forEach((element) => {
-        const li = document.createElement("li");
-        li.textContent = element;
-        list.appendChild(li);
-    });
+    compute(arr);
+    for (let index = 0; index < wordList.length; index++) {
+        const element = wordList[index];
+        const tr = document.createElement("tr");
+        const td1 = document.createElement("td");
+        const td2 = document.createElement("td");
+        const td3 = document.createElement("td");
+        td1.textContent = index + 1;
+        td2.textContent = element;
+        td3.textContent = occurrenceList[index];
+        tr.appendChild(td1);
+        tr.appendChild(td2);
+        tr.appendChild(td3);
+        tbody.appendChild(tr);
+    }
+    table.appendChild(tbody);
+
     remark.textContent = "All Words Converted to LowerCase";
-    body.insertBefore(list, footer);
-    body.insertBefore(remark, list);
+    body.insertBefore(table, footer);
+    body.insertBefore(remark, table);
 }
 
 function reset() {
@@ -73,4 +89,29 @@ function reset() {
     try {
         body.removeChild(list);
     } catch (error) {}
+    try {
+        table.removeChild(tbody);
+    } catch (error) {}
+    tbody = document.createElement("tbody");
+    table.style.display = "none";
+    wordList = [];
+    occurrenceList = [];
+}
+/**
+ *
+ * @param {Array} arr
+ */
+
+function compute(arr) {
+    arr.sort();
+    for (let index = 0; index < arr.length; index++) {
+        const element = arr[index];
+        if (wordList.includes(element)) {
+            let i = wordList.indexOf(element);
+            occurrenceList[i] = occurrenceList[i] + 1;
+        } else {
+            wordList.push(element);
+            occurrenceList.push(1);
+        }
+    }
 }
